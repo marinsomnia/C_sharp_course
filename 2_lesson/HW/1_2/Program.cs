83abdf6f5985f8a64f01db822bc9ffdb916fd125
Mariia <utkaeva.maria@yandex.ru>
2022-12-01 00:40:17 +0300

    HW 1_2

---
+++ 2_lesson/HW/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 2:
+//Напишите программу, которая выводит случайное
+//трёхзначное число и удаляет вторую цифру этого числа.
+
+string Num(int num)
+
+{
+    if ((- 1000 < num && num < - 99) || ( num < 1000 && num > 99))
+    Console.WriteLine(num);
+    return $"{num} -> {num/100*10 + num % 10}"; 
+}
+
+Console.WriteLine(Num(new Random().Next(100,1000)));
