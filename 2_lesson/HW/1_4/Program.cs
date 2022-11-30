e3336196832d272f8b253eb90940125aa0d12cc0
Mariia <utkaeva.maria@yandex.ru>
2022-12-01 00:43:44 +0300

    HW 1_4

---
+++ 2_lesson/HW/1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4:
+//Напишите программу, которая принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+string Weekend(int num)
+{
+    if (num == 6 || num == 7)
+    return "Выходной день!";
+    else
+    return "Будний день";
+}
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(Weekend(number));
