72178a6c42dad15f1567a543630e3be170bb8108
Mariia <utkaeva.maria@yandex.ru>
2022-12-05 23:07:16 +0300

    Work 1_1

---
+++ 4_lesson/HW/1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Без модуля Math, используем цикл for.
+
+Console.WriteLine ("Write a number A: ");
+int num1 = int.Parse (Console.ReadLine ());
+Console.WriteLine ("Write a number B: ");
+int num2 = int.Parse (Console.ReadLine ());
+
+int NatureSteppe (int A, int B)
+{
+    int steppe = 1;
+
+    for (int i = 1; i <= B; i++)
+    {
+        steppe = steppe * A;
+    }
+
+    return steppe;
+}
+
+
+Console.WriteLine(NatureSteppe(num1, num2));
