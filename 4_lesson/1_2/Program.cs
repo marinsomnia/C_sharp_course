1056c94d764cf8a970ce86ae167801cc5106e75a
Mariia <utkaeva.maria@yandex.ru>
2022-12-05 15:29:17 +0300

    Task 1_2

---
+++ 4_lesson/1_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 2. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+Console.WriteLine("Write a number ");
+
+int Proiz(int num)
+{
+int all_sum = 1;
+for (int i = 1; i <= num; i++)
+{
+all_sum = all_sum * i;
+}
+return all_sum;
+}
+
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
