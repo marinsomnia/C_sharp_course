80c6ec0333690181c519faa8723a948f48d50b97
Mariia <utkaeva.maria@yandex.ru>
2022-12-07 00:25:25 +0300

    Work 1_3

---
+++ 4_lesson/HW/1_3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+Console.WriteLine("Write a number of elements massiv: ");
+
+void PrintArray(int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine(mas[i]);
+    }
+}
+
+void FillArray(int[] massiv)
+{
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = new Random().Next(1, 100);
+    }
+}
+
+int[] array = new int[int.Parse(Console.ReadLine())];
+FillArray(array);
+PrintArray(array);
