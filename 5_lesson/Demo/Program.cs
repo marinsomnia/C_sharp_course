808a6b32c4c5d494c2e8a921dad92023b323b79d
Mariia <utkaeva.maria@yandex.ru>
2022-12-07 21:37:51 +0300

    Demo

---
+++ 5_lesson/Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+void Print(int[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+Console.Write($"{arr[i]} ");
+Console.WriteLine();
+}
+
+int[] EightMass(int size)
+{
+int[] arr = new int[size];
+
+for (int i = 0; i < size; i++)
+arr[i] = new Random().Next(1, 100);
+return arr;
+}
+
+int[] arr_1 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_1);
+int[] arr_2 = EightMass(int.Parse(Console.ReadLine()));
+Print(arr_2);
