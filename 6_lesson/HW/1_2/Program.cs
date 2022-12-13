3d30ce2c68ad4f022dccfcde7654e3880100ce60
Mariia <utkaeva.maria@yandex.ru>
2022-12-13 15:41:01 +0300

    HW 1_2

---
+++ 6_lesson/HW/1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 2: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+void Tocka(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k2 * x + b2;
+
+    Console.WriteLine($"Точка пересечения: X: {x}, Y: {y}");
+}
+
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine());
+Tocka(b1, k1, b2, k2);
