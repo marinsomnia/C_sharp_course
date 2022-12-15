708c7e46ce9c3d692829d7ca3e12da0f1e373489
Mariia <utkaeva.maria@yandex.ru>
2022-12-16 02:23:16 +0300

    HW 1_3

---
+++ 7_lesson/HW/1_3/Program.cs
@@ -0,0 +1,57 @@
+﻿//Задача 3. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+void SredneeArifm(int[,] arr)
+{
+
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    double sum = 0;
+
+    for (int i = 0; i < column; i++)
+    {   
+        sum = 0;
+        for (int j = 0; j < row; j++)
+            sum += arr[j, i];
+
+        Console.Write($"{Math.Round(sum / row, 2)}; ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter a range of numbers:");
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Print(arr_1);
+Console.WriteLine("The Average of each: ");
+SredneeArifm(arr_1);
